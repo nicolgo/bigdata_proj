ed4568112f62b2d5e5dfe56ad083baadce3d7ba6
Jiaqi Gu <919659233@qq.com>
2021-11-16 02:54:34 +0800

    update DNN (about 41% acc)

--- DNN.py
+++ DNN.py
@@ -3,70 +3,83 @@ import torch.nn as nn
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import torch.utils.data as Data
+from imblearn.over_sampling import SMOTENC
 pd.options.mode.chained_assignment = None
+
 
 class NeuralNet(nn.Module):
     def __init__(self):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(7, 100)
-        self.dropout = nn.Dropout(0.5)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(100, 3)
+        self.fc1 = nn.Linear(10, 256)
+        self.relu = nn.LeakyReLU()
+        self.fc2 = nn.Linear(256, 256)
+        self.relu2 = nn.LeakyReLU()
+        self.fc3 = nn.Linear(256, 256)
+        self.relu3 = nn.LeakyReLU()
+        self.fc4 = nn.Linear(256, 256)
+        self.relu4 = nn.LeakyReLU()
+        self.fc5 = nn.Linear(256, 256)
+        self.relu5 = nn.LeakyReLU()
+        self.fc6 = nn.Linear(256, 10)
+        self.dropout = nn.Dropout(p=0)
         self.type = 'MLP'
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.dropout(x)
         x = self.relu(x)
-        out = self.fc2(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        x = self.relu3(x)
+        x = self.fc4(x)
+        x = self.relu4(x)
+        x = self.fc5(x)
+        x = self.relu5(x)
+        x = self.fc6(x)
+        out = self.dropout(x)
         return out
 
 
 def create_dataloader():
-
-    data0 = pd.read_csv('../BankChurners.csv')
+    data0 = pd.read_csv('BankChurners.csv')
     data = data0.copy()
 
-    # remove balance with value 0
-    '''
-    data = data[data['Balance'] != 0]
-    '''
+    print(data.isnull().sum())
+    print(data.describe())
 
-    # train 5-7 respectively
-    '''
-    data = data[(data['CreditLevel'] >= 5) & (data['CreditLevel'] <= 7)]
-    '''
+    data['Tenure^2'] = data['Tenure'] ** 2
+    data['Balance^2'] = data['Balance'] ** 2
+    data['EstimatedSalary^2'] = data['EstimatedSalary'] ** 2
+    # data['NumOfProducts^2'] = data['NumOfProducts'] ** 2
 
-    x = data.drop(['CreditLevel', 'CustomerId', 'Geography'], axis=1)
+    x0 = data.drop(['CreditLevel', 'CustomerId', 'Geography'], axis=1)
 
     # Feature Scaling / Standard Score
-    x = (x - x.mean()) / x.std()
-    y = data['CreditLevel']
+    x0 = (x0 - x0.mean()) / x0.std()
 
-    # 5-7 cater to one hot
-    '''
-    y[y == 5] = 1
-    y[y == 6] = 2
-    y[y == 7] = 3
-    '''
+    y0 = data['CreditLevel']
+    y0 = y0 - 1  # cater to one-hot
 
-    y[(y >= 1) & (y <= 4)] = 1
-    y[(y >= 5) & (y <= 7)] = 2
-    y[(y >= 8) & (y <= 10)] = 3
-    y = y-1  # To cater to one hot
+    # balance data
+    smote_nc = SMOTENC(categorical_features=[0, 2, 3, 4, 6], random_state=0)
+    x, y = smote_nc.fit_resample(x0, y0)
+
     x = x.astype('float32')
+
     x_dataset = torch.from_numpy(x.values)
     y_dataset = torch.from_numpy(y.values)
+
     dataset = Data.TensorDataset(x_dataset, y_dataset)
-    train_dataset, test_dataset = train_test_split(dataset, test_size=0.3, random_state=42)
+
+    train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=34)
 
     # Data loader
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                               batch_size=64,
+                                               batch_size=32,
                                                shuffle=True)
 
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                              batch_size=64,
+                                              batch_size=32,
                                               shuffle=False)
 
     return train_loader, test_loader
@@ -74,6 +87,8 @@ def create_dataloader():
 
 def train(train_loader, model, criterion, optimizer, num_epochs):
     # Train the model
+    correct = 0
+    total = 0
     total_step = len(train_loader)
     for epoch in range(num_epochs):
         for step, (attribute, credit) in enumerate(train_loader):
@@ -82,14 +97,19 @@ def train(train_loader, model, criterion, optimizer, num_epochs):
             outputs = model(attribute)
             loss = criterion(outputs, credit.long())
 
+            outputs = model(attribute)
+            _, predicted = torch.max(outputs.data, 1)
+            total += credit.size(0)
+            correct += (predicted == credit).sum().item()
+
             # Backward and optimize
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
-            if (step + 1) % 10 == 0:
+            if (step + 1) % 100 == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                    .format(epoch + 1, num_epochs, step + 1, total_step, loss.item()))
+                      .format(epoch + 1, num_epochs, step + 1, total_step, loss.item()))
+    # print('Train accuracy is: {} %'.format(100 * correct / total))
 
 
 def tst(test_loader, model):
@@ -100,6 +120,7 @@ def tst(test_loader, model):
         for attribute, credit in test_loader:
             outputs = model(attribute)
             _, predicted = torch.max(outputs.data, 1)
+            # print(predicted)
             total += credit.size(0)
             correct += (predicted == credit).sum().item()
 
@@ -112,15 +133,13 @@ if __name__ == '__main__':
 
     ### step 2: instantiate neural network and design model
     model = NeuralNet()
-    # model = ConvNet()
 
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     ### step 3: train the model
-    train(train_loader, model, criterion, optimizer, num_epochs=5)
+    train(train_loader, model, criterion, optimizer, num_epochs=12)
 
     ### step 4: test the model
     tst(test_loader, model)
-
