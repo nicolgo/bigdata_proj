6e1d46caed87bc079672ab3ab42f53d32e6f5c67
vincentAffinity <yanyibin@jmgo.com>
2021-12-06 22:51:01 +0800

    tree pic

--- final_version.py
+++ final_version.py
@@ -1,15 +1,22 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import linear_model
 
 from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn import datasets
 from xgboost.sklearn import XGBClassifier
+import pydotplus
+from IPython.display import Image
 import pandas as pd
 
 # 当拓展特征时需要修改此参数 每加一个特征此常量++
-from deep_learning import train_model_outer, predict_test_class, predict_output
+from bigdata.deep_learning import train_model_outer, predict_test_class, predict_output
 
-drop_col_index = 8 + 3
+drop_col_index = 8 + 3 - 3
 
 
 def read_data(file_name='BankChurners.csv'):
@@ -156,9 +163,9 @@ def combine_feature(data_set):
                 feat3.append(3)
             else:
                 feat3.append(4)
-    data_set['NewFeature1'] = feat1
-    data_set['NewFeature2'] = feat2
-    data_set['NewFeature3'] = feat3
+    # data_set['NewFeature1'] = feat1
+    # data_set['NewFeature2'] = feat2
+    # data_set['NewFeature3'] = feat3
     return data_set
 
 
@@ -246,6 +253,7 @@ def fit_level_classifier(data_set):
         min_child_weight=1)
 
     model.fit(x_train, y_train, eval_metric='auc')
+    # model.fit(x_train, y_train)
     return model
 
 
@@ -340,7 +348,7 @@ def fit_sub_level(model, train_data_set, xgb=False, l2=False):
     x_train = train_data_set.drop(columns=drop_col_index).drop(columns=drop_col_index + 1)
     if l2:
         from imblearn.over_sampling import SVMSMOTE
-        smote_nc = SVMSMOTE(sampling_strategy={4: 1700, 5: 1700, 6: 1700, 7: 1700, 8: 1700}, random_state=0)
+        smote_nc = SVMSMOTE(sampling_strategy={4: 2000, 5: 2000, 6: 2000, 7: 2000, 8: 2000}, random_state=0)
         x_train, y_train = smote_nc.fit_resample(x_train, y_train)
     if xgb:
         model.fit(x_train, y_train, eval_metric='auc')
@@ -357,6 +365,23 @@ def predict_sub_credit(model, test_data):
     :return: 预测的credit
     """
     return model.predict(test_data)
+
+
+def collect_data(data):
+    res = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for item in data:
+        if item == 4:
+            res[3] += 1
+        elif item == 5:
+            res[4] += 1
+        elif item == 6:
+            res[5] += 1
+        elif item == 7:
+            res[6] += 1
+        elif item == 8:
+            res[7] += 1
+    print(res)
+    return res
 
 
 if __name__ == '__main__':
@@ -376,25 +401,10 @@ if __name__ == '__main__':
     l1_dataset, l2_dataset, l3_dataset = divide_dataset_2_levels(data_set=d_train_set)
 
     # 3 sub-class model init
-    l1_model = LogisticRegression()
     l1_model = RandomForestClassifier(n_estimators=400)
-    l2_model = XGBClassifier(
-        # 树的个数
-        n_estimators=200, learning_rate=0.08, max_depth=15, subsample=1, seed=1000,
-        # 用于控制是否后剪枝的参数,越大越保守，一般0.1、0.2这样子
-        gamma=0,
-        # 控制模型复杂度的权重值的L2正则化项参数，参数越大，模型越不容易过拟合。
-        reg_lambda=1,
-        # 最大增量步长，我们允许每个树的权重估计。
-        max_delta_step=0,
-        # 生成树时进行的列采样
-        colsample_bytree=1,
-        # 这个参数默认是 1，是每个叶子里面 h 的和至少是多少，对正负样本不均衡时的 0-1 分类而言
-        # 假设 h 在 0.01 附近，min_child_weight 为 1 意味着叶子节点中最少需要包含 100 个样本。
-        # 这个参数非常影响结果，控制叶子节点中二阶导的和的最小值，该参数值越小，越容易 过拟合。
-        min_child_weight=1
-    )
     l2_model = RandomForestClassifier(n_estimators=600)
+    # l2_model = LogisticRegression()
+    # l2_model = LinearSVC()
     l3_model = RandomForestClassifier(n_estimators=400)
 
     # train
@@ -402,7 +412,24 @@ if __name__ == '__main__':
     l2_model = fit_sub_level(model=l2_model, train_data_set=l2_dataset, l2=False)
     l3_model = fit_sub_level(model=l3_model, train_data_set=l3_dataset)
 
-    # d_test_set = predict_level_classification(model=l_model, test_data=d_test_set)
+    # Estimators = l2_model.estimators_
+    # for index, model in enumerate(Estimators):
+    #     filename = 'iris_' + str(index) + '.pdf'
+    #     dot_data = tree.export_graphviz(model, out_file=None,
+    #                                     feature_names=['geo',
+    #                                     'tenure', 'balance',
+    #                                     'NumOfProducts', 'HasCrCard',
+    #                                     'IsActiveMember', 'EstimatedSalary',
+    #                                     'Exited'],
+    #                                     class_names=['4', '5', '6', '7', '8'],
+    #                                     filled=True, rounded=True,
+    #                                     special_characters=True)
+    #     graph = pydotplus.graph_from_dot_data(dot_data)
+    #     # 使用ipython的终端jupyter notebook显示。
+    #     Image(graph.create_png())
+    #     graph.write_pdf(filename)
+
+
     d_test_set['c_level'] = predict_test_class(l_model, test_set)
     t_l1_dataset, t_l2_dataset, t_l3_dataset, dic = divide_dataset_2_levels_test(data_set=d_test_set)
 
@@ -417,11 +444,14 @@ if __name__ == '__main__':
             if int(t_l1_credit_answer[i]) == int(predict_l1[i]):
                 right_number += 1
 
+    data_res = []
     if not t_l2_dataset.empty:
         t_l2_credit_answer = t_l2_dataset[drop_col_index]
         t_l2_dataset = t_l2_dataset.drop(columns=drop_col_index).drop(columns=drop_col_index + 1)
         predict_l2 = predict_sub_credit(model=l2_model, test_data=t_l2_dataset)
         for i in range(0, len(t_l2_credit_answer)):
+            data_res.append(int(predict_l2[i]))
+            # print(str(t_l2_credit_answer[i]) + " " + str(predict_l2[i]))
             if int(t_l2_credit_answer[i]) == int(predict_l2[i]):
                 right_number += 1
 
@@ -434,43 +464,5 @@ if __name__ == '__main__':
                 right_number += 1
 
     sum_number = len(d_test_set)
-    print(right_number / sum_number)
+    print("Accuracy for all data: ", right_number / sum_number)
 
-    ################################################################################################
-    # output
-    final_test_data = pd.read_csv('New_BankChurners.csv')
-    final_test_data = final_test_data.copy()
-
-    final_test_data = predict_balance_output(data_set=final_test_data)
-    final_test_data = combine_feature(data_set=final_test_data)
-    final_test_data = data_basic_clean(data_set=final_test_data)
-
-    final_test_data['c_level'] = predict_output(l_model)
-    t_l1_dataset, t_l2_dataset, t_l3_dataset, dic = divide_dataset_2_levels_test(data_set=final_test_data)
-    res_arr = [1 for item in range(0, 1000)]
-
-    if not t_l1_dataset.empty:
-        t_l1_credit_answer = t_l1_dataset[drop_col_index]
-        t_l1_dataset = t_l1_dataset.drop(columns=drop_col_index).drop(columns=drop_col_index + 1)
-        predict_l1 = predict_sub_credit(model=l1_model, test_data=t_l1_dataset)
-        for i in range(0, len(t_l1_credit_answer)):
-            res_arr[dic['1_' + str(i)]] = int(predict_l1[i])
-
-    if not t_l2_dataset.empty:
-        t_l2_credit_answer = t_l2_dataset[drop_col_index]
-        t_l2_dataset = t_l2_dataset.drop(columns=drop_col_index).drop(columns=drop_col_index + 1)
-        predict_l2 = predict_sub_credit(model=l2_model, test_data=t_l2_dataset)
-        for i in range(0, len(t_l2_credit_answer)):
-            res_arr[dic['2_' + str(i)]] = int(predict_l2[i])
-
-    if not t_l3_dataset.empty:
-        t_l3_credit_answer = t_l3_dataset[drop_col_index]
-        t_l3_dataset = t_l3_dataset.drop(columns=drop_col_index).drop(columns=drop_col_index + 1)
-        predict_l3 = predict_sub_credit(model=l3_model, test_data=t_l3_dataset)
-        for i in range(0, len(t_l3_credit_answer)):
-            res_arr[dic['3_' + str(i)]] = int(predict_l3[i])
-
-    final_test_data = pd.read_csv('New_BankChurners.csv')
-    final_test_data['CreditLevel'] = res_arr
-    final_test_data.to_csv('final_output.csv')
-    print(res_arr)
